e827bbffc09bc5ccea8f78e9d07943a1a2ea3a37
branley1 <mmasi.branley@gmail.com>
2024-11-29 00:37:05 -0500

    fixed dumb typo

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import CustomNavbar from "./components/Navbar/Navbar";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Hero from "./components/HeroSection/HeroSection";
-import AboutMe from "./components/ABoutMe/AboutMe";
+import AboutMe from "./components/AboutMe/AboutMe";
 import ExperienceTimeline from "./components/ExperienceTimeline/ExperienceTimeline";
 import ProjectsShowcase from "./components/Projects/Projects";
 import Footer from "./components/Footer/Footer";
