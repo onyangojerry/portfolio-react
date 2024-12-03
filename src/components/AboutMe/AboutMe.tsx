0a4cfaefe831b4b3113e1b106904349828586e9b
branley1 <mmasi.branley@gmail.com>
2024-12-03 16:55:13 -0500

    fixed about me

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -5,31 +5,32 @@ const AboutMe: React.FC = () => (
   <section id="about" className="my-4 about-me">
     <h2>About Me</h2>
     <p>
-      I'm a senior at Swarthmore College majoring in Computer Science with a
+      I'm a senior at Swarthmore College, majoring in Computer Science with a
       Cognitive Science minor. I specialize in full-stack development, machine
-      learning, and Artificial Intelligence, with a passion for creating
-      innovative, user-focused solutions.
+      learning, and artificial intelligence, with a passion for creating
+      innovative, user-focused solutions that make a real impact.
     </p>
     <p>
-      I'm a software engineer with a strong foundation in full-stack
-      development, and scalable system design. I've specialized in building
-      solutions with tools such as React.js, Node.js, and TensorFlow, and I'm
-      proficient in several languages including Python, C, C++, Swift, GoLang,
-      and TypeScript.
+      As a software engineer, I have a strong foundation in scalable system
+      design and full-stack development. I've built solutions using tools like
+      React.js, Node.js, and TensorFlow, and I'm proficient in several
+      programming languages, including Python, C++, Swift, Go, and TypeScript.
     </p>
     <p>
-      Fascinated by the intersection of technology and creativity, my interests
-      span music, film (animations), 3D development, and human-computer
-      interaction. My work emphasizes AI/ML, natural language processing (NLP),
-      computational linguistics, and full-stack development.
+      I'm deeply fascinated by the intersection of technology and creativity. My
+      interests span music, film (animations), 3D development, and
+      human-computer interaction, inspiring me to explore how technology can
+      enhance artistic expression and user experiences. My work often focuses on
+      AI/ML, natural language processing (NLP), computational linguistics, and
+      full-stack applications.
     </p>
 
     <p>
-      I'm particularly passionate about using NLP to uncover implicit knowledge
-      in large text collections and applying it to build human-centered
-      applications. My goal is to enhance human-computer interactions by
-      bridging the gap between machines and humans for more intuitive and
-      meaningful connections.
+      I'm particularly passionate about using NLP to uncover hidden patterns and
+      knowledge within large text datasets and applying these insights to create
+      human-centered applications. My ultimate goal is to enhance human-computer
+      interactions by bridging the gap between machines and humans, fostering
+      more intuitive and meaningful connections.
     </p>
   </section>
 );
