436516854d7109b7cf2a98be4f8f3108f654f0e0
branley1 <mmasi.branley@gmail.com>
2025-02-12 21:22:52 -0500

    added retro version from fork

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactGA from "react-ga4";
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layouts/Layout";
