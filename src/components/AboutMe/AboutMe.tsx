bed5ecb162be41a038c6299ba573ed6f4ca22712
Jerry Onyango <118232508+onyangojerry@users.noreply.github.com>
2025-02-13 18:30:49 -0800

    Update AboutMe.tsx

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -4,23 +4,22 @@ import "./_about-me.scss";
 const AboutMe: React.FC = () => (
   <section id="about" className="my-4 about-me">
     <h2>Hi there!</h2>
-    <p>🔭 I'm diving into deep learning and machine learning by building an AI chatbot using fine-tuned local LLMs, working on real-world applications and picking up key concepts along the way.</p>
-    <p>🌱 In my Compilers class, I'm learning OCaml and getting hands-on with functional programming. I'm also exploring JUCE to develop audio plugins—exciting stuff!</p>
-    <p>👯 I love collaborating on open source projects, especially those that involve creating interactive websites, iOS apps, or anything that puts users first. Check out my projects on GitHub!</p>
+    <p>🔭 I'm diving into Computer Systems, System Security,Deep learning and Real-Time Systems for Embeded Systems by identifying niche research papers on the same, extrapolating and implementing.</p>
+    <p>🌱 In my Real-Time Systems class, I'm learning further C and C++ and getting hands-on with Managing Complex Systems and Complex Software Projects. I'm also exploring Operating Systems, involving myself in the BSD and overall Unix Systems!</p>
+    <p>👯 I love collaborating on open source projects, especially those that involve user security, or anything that puts users first. Check out my projects on GitHub!</p>
     <h5>My interests mainly lie in: </h5>
     <ul>
       <li>Full-stack (both frontend & backend) development</li>
-      <li>iOS/Android development</li>
-      <li>Machine Learning & Artificial Intelligence</li>
+      <li>Systems</li>
+      <li>Natural Language Processing and Neural Networks/ Deep Learning</li>
       <li>Fintech</li>
-      <li>Cloud Computing & DevOps</li>
+      <li>Distributed systems & DevOps</li>
     </ul>
     <p>
-      🪴 My passion lies in applying natural language processing to uncover
-      implicit patterns and insights within large collections of text, enabling
-      the creation of human-centered applications. My ultimate goal is to
-      enhance human-computer interactions by bridging the gap between machines
-      and humans, fostering intuitive and meaningful connections.
+      🪴 My passion lies in applying natural language processing, neural networks and machine learning algorithms to uncover
+      implicit patterns and insights within large collections of threat detection data, enabling
+      high-end threat monitoring syatems. My ultimate goal is to
+      enhance secure systems by incorporating AI and System security, fostering secure human-computer interaction.
     </p>
   </section>
 );
