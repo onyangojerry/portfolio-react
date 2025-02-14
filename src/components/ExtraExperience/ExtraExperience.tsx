43ea2a4415a4dde23deb219f013285b9bdf4ffc2
Jerry Onyango <118232508+onyangojerry@users.noreply.github.com>
2025-02-13 19:41:46 -0800

    Update ExtraExperience.tsx

--- src/components/ExtraExperience/ExtraExperience.tsx
+++ src/components/ExtraExperience/ExtraExperience.tsx
@@ -41,16 +41,13 @@ const ExperienceTimeline: React.FC = () => (
     <h2>Extracurricular Experience</h2>
     <ExperienceItem
       role="
-      Swarthmore College Fellow 2024"
-      company="University Innovation Fellows"
-      duration="Jul 2023 - Jun 2024"
-      location="Swarthmore, PA"
+      Equity Global Scholars Mentor"
+      company="Equity Group KE"
+      duration="Jul 2021 - March 2022"
+      location="Nairobi, KE"
       description={[
-        "Led campus-wide initiatives to enhance student engagement in innovation and entrepreneurship, impacting over 500 students.",
+        "Guided a group of top performing students through the college application process helping them secure admissions to top colleges in the United States and other parts of the globe.",
 
-        "Developed strategies to improve collaboration among 15+ student organizations, boosting cross-organizational events by 60%.",
-
-        "Presented research findings at the UIF conference in the Netherlands, leading to the implementation of 5+ campus-wide changes.",
       ]}
     />
     <ExperienceItem
@@ -64,52 +61,7 @@ const ExperienceTimeline: React.FC = () => (
         "Demonstrated ethical leadership through confidential peer counseling, conflict resolution, and crisis management.",
       ]}
     />
-    <ExperienceItem
-      role="Multicultural Recruitment Intern"
-      company="Swarthmore College"
-      duration="Oct 2021 - Oct 2024"
-      location="Swarthmore, PA"
-      description={[
-        "Worked closely with the Admissions Office Access Team to recruit underrepresented populations, enhancing campus diversity.",
-
-        "Recruited and trained 50+ student hosts, tour guides, and event workers for admissions activities.",
-
-        "Directed key recruitment and yield events, including Discover Swarthmore and Swatstruck, engaging 500+ prospective students.",
-      ]}
-    />
-    <ExperienceItem
-      role="Student Admissions' Tour Guide"
-      company="Swarthmore College"
-      location="Swarthmore, PA"
-      duration="May 2023 - Aug 2023"
-      description={[
-        "Maintained detailed tour schedules and managed groups of up to 30 visitors, ensuring timely tour starts.",
-
-        "Conducted in-depth research on the college's history, academic offerings, student organizations, and campus facilities to evaluate and bolster resource availability to promote inclusivity and leadership throughout all campus communities.",
-
-        "Delivered informative and engaging campus tours for over 200 prospective students and families per month.",
-      ]}
-    />
-    <ExperienceItem
-      role="Game Day Operator"
-      company="Swarthmore College"
-      location="Swarthmore, PA"
-      duration="Oct 2021 - May 2022"
-      description={[
-        "Collaborated with other game day operators to handle all aspects of the game, including the music, video production, camera operations, statistics, photography, scoreboard management, ball chasers, and other activities as designated by the Office of Athletic Communications at Swarthmore College.",
-      ]}
-    />
-    <ExperienceItem
-      role="Academic Tutor"
-      company="Amiva"
-      location="Nairobi, Kenya"
-      duration="Mar 2022 - Nov 2022"
-      description={[
-        "Served as an academic tutor to high school students in Kenya applying to US universities and colleges.",
-
-        "Mentored 10+ students through essay writing, helping them practice for aptitude tests and creating a comprehensive college application.",
-      ]}
-    />
+   
   </section>
 );
 
