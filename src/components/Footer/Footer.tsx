aeb2b2f5ea5d6d13c7d7823debab3404a189775c
Jerry Onyango <118232508+onyangojerry@users.noreply.github.com>
2025-02-13 19:58:50 -0800

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,9 +4,9 @@ import "/src/components/Footer/_footer.scss";
 const Footer: React.FC = () => (
   <footer className="footer">
     <p>
-      &copy; {new Date().getFullYear()} Branley Mmasi | Connect on{" "}
-      <a href="https://linkedin.com/in/bmmasi1">LinkedIn</a> or{" "}
-      <a href="https://github.com/branley1">GitHub</a>.
+      &copy; {new Date().getFullYear()} Jerry Onyango | Connect on{" "}
+      <a href="https://www.linkedin.com/in/jerry-rawlings-onyango/.">LinkedIn</a> or{" "}
+      <a href="https://https://github.com/onyangojerry">GitHub</a>.
     </p>
   </footer>
 );
