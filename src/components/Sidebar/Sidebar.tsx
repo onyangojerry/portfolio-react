808cd3ef2c17c05e9bbf6a2b6872952b846fe698
Jerry Onyango <118232508+onyangojerry@users.noreply.github.com>
2025-02-18 13:47:33 -0800

    Update Sidebar.tsx

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -15,7 +15,7 @@ const Sidebar: React.FC<SidebarProps> = ({ className = "" }) => (
       <h5>
         Current Mood 🎧
         <a><i>
-            <img src="https://spotify-github-profile.vercel.app/api/view.svg?uid=xafrumx47d4e0kygeqpyqpsx8&redirect=true&cover_image=true&theme=default&show_offline=true&background_color=121212&interchange=true&bar_color=53b14f&bar_color_cover=false)](https://spotify-github-profile.kittinanx.com/api/view.svg?uid=hvoh3gwfkd3h64bzeal1fejmu&redirect=true)"></img>
+            <img src="https://spotify-github-profile.vercel.app/api/view.svg?uid=xafrumx47d4e0kygeqpyqpsx8&redirect=true&cover_image=true&theme=default&show_offline=true&background_color=121212&interchange=true&bar_color=53b14f&bar_color_cover=false)](https://spotify-github-profile.kittinanx.com/api/view.svg?uid=xafrumx47d4e0kygeqpyqpsx8&redirect=true)"></img>
             </i>
           </a>
       </h5>
